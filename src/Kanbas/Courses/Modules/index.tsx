96b9f61685f9d15851bffe3c58e478b233221537
monshanlin <monshanlin@gmail.com>
2024-07-08 23:32:20 +0800

    Add course description

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -12,6 +12,11 @@ export default function Modules() {
                   <li className="wd-content-item">Introduction to the course</li>
                   <li className="wd-content-item">Learn what is Web Development</li>
                 </ul>
+                <span className="wd-title">READING</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Full Stack Developer - CH 1</li>
+                  <li className="wd-content-item">Full Stack Developer - CH 2</li>
+                </ul>
               </li>
             </ul>
           </li>
@@ -20,6 +25,15 @@ export default function Modules() {
             <ul className="wd-lessons">
               <li className="wd-lesson">
                 <span className="wd-title">LEARNING OBJECTIVES</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Formulate User Interface with HTML</li>
+                  <li className="wd-content-item">Learn how to create user interface with HTML</li>
+                </ul>
+                <span className="wd-title">READING</span>
+                <ul className="wd-content">
+                  <li className="wd-content-item">Full Stack Developer - CH 3</li>
+                  <li className="wd-content-item">Full Stack Developer - CH 4</li>
+                </ul>
               </li>
             </ul>
           </li>
