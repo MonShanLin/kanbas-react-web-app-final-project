91b838e889daba111e3a011831b82e8be0dcc943
monshanlin <monshanlin@gmail.com>
2024-07-16 03:14:56 +0800

    change github ID attribute to  wd-github

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,7 +8,6 @@ export default function Labs() {
     <div>
       <h1>Mon-Shan Lin</h1>
       <h1>CS5610 Summer 2 Section: 61065</h1>
-      <h1>https://github.com/MonShanLin/kanbas-react-web-app </h1>
       <h1>Labs</h1>
       <TOC />
       <Routes>
