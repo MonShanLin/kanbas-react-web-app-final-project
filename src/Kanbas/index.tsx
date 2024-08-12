65fdbdfc92a400edf9cdcb984605743d2d5d2840
monshanlin <monshanlin@gmail.com>
2024-08-13 05:43:06 +0800

    third  commit

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -7,7 +7,8 @@ import KanbasNavigation from "./Navigation";
 import Courses from "./Courses";
 import * as client from "./Courses/client";
 import "./styles.css";
-import axios from "axios";
+import Account from "./Account";
+import ProtectedRoute from "./ProtectedRoute";
 
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
@@ -54,18 +55,22 @@ export default function Kanbas() {
         <div className="wd-main-content-offset p-3">
           <Routes>
             <Route path="/" element={<Navigate to="Dashboard" />} />
-            <Route path="Account" element={<h1>Account</h1>} />
+            <Route path="/Account/*" element={<Account />} />
             <Route path="Dashboard" element={
-              <Dashboard
+              <ProtectedRoute><Dashboard
                 courses={courses}
                 course={course}
                 setCourse={setCourse}
                 addNewCourse={addNewCourse}
                 deleteCourse={deleteCourse}
                 updateCourse={updateCourse}
-              />
+              /></ProtectedRoute>
             } />
-            <Route path="Courses/:cid/*" element={<Courses courses={courses} />} />
+            <Route path="Courses/:cid/*" element={
+              <ProtectedRoute><Courses 
+              courses={courses} 
+              /></ProtectedRoute>} />
+              
             <Route path="Calendar" element={<h1>Calendar</h1>} />
             <Route path="Inbox" element={<h1>Inbox</h1>} />
           </Routes>
